35dbd023006b2bbbd0e7ca6ca9890a5163335e54
sindhu-sp <sindhupillaisuresh@gmail.com>
2021-08-10 20:33:58 -0700

    post-router added

--- models/Post.js
+++ models/Post.js
@@ -16,12 +16,12 @@ Post.init(
         type: DataTypes.STRING,
         allowNull: false
       },
-      post_url: {
+      post_content: {
         type: DataTypes.STRING,
         allowNull: false,
-        validate: {
-          isURL: true
-        }
+        // validate: {
+        //   isURL: true
+        // }
       },
       user_id: {
         type: DataTypes.INTEGER,
