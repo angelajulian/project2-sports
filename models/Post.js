71f75dcfe45812cded3db19c06dcfc483645db6b
sindhu-sp <sindhupillaisuresh@gmail.com>
2021-08-11 22:17:59 -0700

    models Game Post Sport added

--- models/Post.js
+++ models/Post.js
@@ -1,42 +1,76 @@
-const { Model, DataTypes } = require('sequelize');
+const {
+  Model,
+  DataTypes
+} = require('sequelize');
 const sequelize = require('../config/connection');
 // create our Post model
 class Post extends Model {}
 
 // create fields/columns for Post model
-Post.init(
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      post_content: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        // validate: {
-        //   isURL: true
-        // }
-      },
-      user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-          model: 'user',
-          key: 'id'
-        }
-      }
-    },
-    {
-      sequelize,
-      freezeTableName: true,
-      underscored: true,
-      modelName: 'post'
+Post.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  post_content: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    // validate: {
+    //   isURL: true
+    // }
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'user',
+      key: 'id'
     }
-  );
-  
-  module.exports = Post;
+  },
+  game_id: {
+    type: DataTypes.INTEGER,
+    references: {
+      model: 'game',
+      key: 'id'
+    }
+  },
+
+time_created: {
+  type: DataTypes.DATE,
+  allowNull: false,
+  validate: {
+    
+  }
+},
+}, 
+{
+  sequelize,
+  freezeTableName: true,
+  underscored: true,
+  modelName: 'post'
+});
+
+module.exports = Post;
+
+
+// var Person = sequelize.define('Person', {
+//   name: Sequelize.STRING
+// });
+
+// var Father = sequelize.define('Father', {
+
+//   age: Sequelize.STRING,
+//   //The magic start here
+//   personId: {
+//     type: Sequelize.INTEGER,
+//     references: 'person', // <<< Note, its table's name, not object name
+//     referencesKey: 'id' // <<< Note, its a column name
+//   }
+// });
+
+// Person.hasMany(Father); // Set one to many relationship
--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
-const { Model, DataTypes } = require("sequelize");
+const {
+  Model,
+  DataTypes
+} = require("sequelize");
 const bcrypt = require("bcrypt");
 const sequelize = require("../config/connection");
 
@@ -11,63 +14,64 @@ class User extends Model {
 }
 
 // create fields/columns for User model
-User.init(
-  {
-    //Table column definitions
-    id: {
+User.init({
+  //Table column definitions
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: DataTypes.STRING,
+    validate: {
+      isEmail: true,
+    },
+    unique: true,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      // TODO: include password complexity checks later
+      len: [6],
+    },
+    area_code: {
       type: DataTypes.INTEGER,
       allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING,
-      validate: {
-        isEmail: true,
-      },
-      unique: true,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        // TODO: include password complexity checks later
-        len: [6],
-      },
-    },
-    // blocklist: {
-    //     type: DataTypes.ARRAY
-    //     //validate array of integers
-    // }
-    //TODO: birthdate and age gating?
+    }
   },
-  {
-    // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-    hooks: {
-      // set up beforeCreate lifecycle "hook" functionality
-      async beforeCreate(newUserData) {
-        newUserData.password = await bcrypt.hash(newUserData.password, 10);
-        return newUserData;
-      },
-
-      async beforeUpdate(updatedUserData) {
-        updatedUserData.password = await bcrypt.hash(
-          updatedUserData.password,
-          10
-        );
-        return updatedUserData;
-      },
+  // blocklist: {
+  //     type: DataTypes.ARRAY
+  //     //validate array of integers
+  // }
+  //TODO: birthdate and age gating?
+}, {
+  // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
+  hooks: {
+    // set up beforeCreate lifecycle "hook" functionality
+    async beforeCreate(newUserData) {
+      newUserData.password = await bcrypt.hash(newUserData.password, 10);
+      return newUserData;
     },
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: "user",
-  }
-);
+
+    async beforeUpdate(updatedUserData) {
+      updatedUserData.password = await bcrypt.hash(
+        updatedUserData.password,
+        10
+      );
+      return updatedUserData;
+    },
+  },
+  sequelize,
+  freezeTableName: true,
+  underscored: true,
+  modelName: "user",
+});
 
 module.exports = User;
