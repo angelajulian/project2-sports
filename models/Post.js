f666764fc84d60a04a5232bae518e1f21861b790
Julian <angela.julian@gmail.com>
2021-08-13 19:16:30 -0700

    changes for Sindhu's code review

--- models/Post.js
+++ models/Post.js
@@ -1,62 +1,57 @@
-const {
-  Model,
-  DataTypes
-} = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 // create our Post model
 class Post extends Model {}
 
 // create fields/columns for Post model
-Post.init({
-  id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  title: {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  post_content: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    // validate: {
-    //   isURL: true
-    // }
-  },
-  user_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: 'user',
-      key: 'id'
-    }
-  },
-  game_id: {
-    type: DataTypes.INTEGER,
-    references: {
-      model: 'game',
-      key: 'id'
-    }
-  },
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    post_content: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      // validate: {
+      //   isURL: true
+      // }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    game_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "game",
+        key: "id",
+      },
+    },
 
-time_created: {
-  type: DataTypes.DATE,
-  allowNull: false,
-  validate: {
-    
+    time_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "post",
   }
-},
-}, 
-{
-  sequelize,
-  freezeTableName: true,
-  underscored: true,
-  modelName: 'post'
-});
+);
 
 module.exports = Post;
-
 
 // var Person = sequelize.define('Person', {
 //   name: Sequelize.STRING
