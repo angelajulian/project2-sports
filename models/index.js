ad1eff0111ccb3708896e74c97816c6f0abad329
sindhu-sp <sindhupillaisuresh@gmail.com>
2021-08-14 23:56:44 -0700

    index.js updated in the models

--- models/index.js
+++ models/index.js
@@ -5,5 +5,25 @@ const Sport = require("./Sport");
 const Game = require("./Game");
 
 
+ Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "SET NULL",
+});
 
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL'
+  });
+Game.hasMany(Post, {
+    foreignKey: 'game_id',
+    onDelete: "CASCADE"
+});
+User.hasMany(Game, {
+    foreignKey: 'user_id',
+    onDelete: "SET NULL"
+});
+Sport.hasMany(Game, {
+    foreignKey: 'sport_id',
+    onDelete: 'CASCADE'
+})
 module.exports = { User, Post, Sport, Game };
