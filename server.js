eef5995e3efcaf26126360b98e9fde3a4de78e82
andrew lopez <andrewlpez092@gmail.com>
2021-08-17 14:13:39 -0700

    added handle bars to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
 const sequelize = require("./config/connection");
+const exphbs = require( 'express-handlebars')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,4 +15,12 @@ app.use(routes);
 // turn on connection to db and server
 sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log(`Now listening at ${PORT}`));
+});
+
+// set Handlebars Middleware
+app.engine('handlebars', exphbs)
+app.set('view engine', 'handlebars')
+
+app.get('/', function (req, res) {
+  res.render('home');
 });
